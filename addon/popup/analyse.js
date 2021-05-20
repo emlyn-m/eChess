770438897e067b04f6c558c758ea46a2618cab4b
Emlyn Matheson <emlynmatheson@gmail.com>
2021-05-20 17:54:14 +1200

    Remove server as all analysis will now be performed locally

--- addon/popup/analyse.js
+++ addon/popup/analyse.js
@@ -81,3 +81,35 @@ function sanToUci(sanArr) {
     whiteMove = !(whiteMove);
   }
 }
+
+function moveAnalysis(move, engineTxt, preEngineTxt) {
+  //Return values:
+    //Book {OPENING}: Move is a book move from the opening OPENING
+    //Missed: Move is a missed win
+    //Best: Move is best move
+    //Brilliant: Move is a brilliant move
+    //Excellent: Move is an excellent move
+    //Good: Move is a good move
+    //Inaccuracy: Move is an inaccuracy
+    //Mistake: Move is a mistake
+    //Blunder: Move is a blunder
+
+}
+
+function gameAnalysis() {
+  const engineCommands = {
+    "initialize": "uci\nsetoption name Hash value "
+
+    "readyAsk": "isready"
+    "readyRes": "readyok"
+
+    "newGameAnalyse": "ucinewgame\nsetoption name UCI_AnalyseMode value true"
+
+    "loadPos": "position startpos moves" //Moves should begin with a space and be given in long algebraic notation
+    "search": "go depth "
+  };
+
+  //Engine constants
+  const HashValue = 32;
+  const OpeningTablePath = ""; //TODO: Download ECO, implement ECO search
+}
