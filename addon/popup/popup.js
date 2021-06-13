a889149cf6d90d3e9126cdf6280a79b1a92e56d6
Emlyn Matheson <emlynmatheson@gmail.com>
2021-06-13 20:30:05 +1200

    I give up imma j use a lib

--- addon/popup/popup.js
+++ addon/popup/popup.js
@@ -10,75 +10,8 @@ function listenForClicks() {
   })
 }
 
-function sanToUci(sanArr) {
-  whiteMove = true //Boolean : True - White, False - Black
+function pgnToUci(pgnStr) {
 
-  boardState = [
-    ["R", "N", "B", "Q", "K", "B", "N", "R"],
-    ["P", "P", "P", "P", "P", "P", "P", "P"],
-    [" ", " ", " ", " ", " ", " ", " ", " "],
-    [" ", " ", " ", " ", " ", " ", " ", " "],
-    [" ", " ", " ", " ", " ", " ", " ", " "],
-    [" ", " ", " ", " ", " ", " ", " ", " "],
-    ["p", "p", "p", "p", "p", "p", "p", "p"],
-    ["r", "n", "b", "q", "k", "b", "n", "r"]
-  ] //Top Left (0, 0) = A8
-  //TODO: Look into using bitboards
-
-  uciArray = [];
-
-  for (move in sanArr) {
-    newMove = move.replace('x', '')
-    destSq = newMove.slice(-2)
-
-    uciMove = ""
-
-    if (newMove == "O-O") {
-      uciMove = `e${whiteMove*-7+8}h${whiteMove*-7+8}`
-    } else if (newMove = "O-O-O") {
-      uciMove = `e${whiteMove*-7+8}a${whiteMove*-7+8}`
-    } else {
-
-      if (isPawnMove) {
-
-        //Check for promotion
-        switch(newMove.length) {
-          case 5:
-            //Capture and promotion
-          case 4:
-            //No capture, promotion
-            uciMove = newMove.charAt(0)+(newMove.charAt(1)-1)+newMove; //Prepend former square
-            uciMove = uciMove.replace("=", "").toLowerCase();
-
-            //TODO: Update board state
-
-            break;
-          case 3:
-            //Capture, no promotion
-          case 2:
-            //No capture or promotion
-        }
-
-      } else {
-
-        switch (newMove.length) {
-          case 5:
-            //TODO: Update board state
-            uciMove = newMove.slice(-4);
-            break;
-          case 4:
-            //TODO: Stuff
-            break;
-          case 3:
-            //TODO: Stuff
-        }
-
-      }
-
-    }
-
-    uciArray.push(uciMove);
-    whiteMove = !(whiteMove);
   }
 }
 
--- debug.js
+++
@@ -1,2 +0,0 @@
-import * as king from 'king.js';
-import * as knight from 'knight.js'
